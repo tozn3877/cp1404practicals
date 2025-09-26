0d2429eb47599a9e5a7bd518f7a80853a9e01334
tozn3877 <torindale05@gmail.com>
2025-09-26 16:37:23 +1000

    get password function added

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -9,11 +9,15 @@ MIN_PASSWORD_LENGTH = 8
 
 def main():
     """Get a valid password and print asterisks as long as the password."""
+    password = get_password()
+    print('*' * len(password))
+
+def get_password():
+    """Prompt user until they enter a password of at least MIN_PASSWORD_LENGTH characters."""
     password = input("Enter password: ")
     while len(password) < MIN_PASSWORD_LENGTH:
         print(f"Password must be at least {MIN_PASSWORD_LENGTH} characters long.")
         password = input("Enter password: ")
-    print('*' * len(password))
-
+    return password
 
 main()
