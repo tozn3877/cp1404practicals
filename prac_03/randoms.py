19ef466a07e0905a6e85bec5f6b81207ff298e58
tozn3877 <torindale05@gmail.com>
2025-10-02 14:31:50 +1000

    add starter code

---
+++ prac_03/randoms.py
@@ -0,0 +1,10 @@
+"""
+CP1404/CP5632 - Practical
+Random numbers examples
+"""
+
+import random
+
+print(random.randint(5, 20))  # line 1
+print(random.randrange(3, 10, 2))  # line 2
+print(random.uniform(2.5, 5.5))  # line 3
