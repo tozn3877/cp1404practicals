3d2202a737a12a600c29aca72d8ae3e3d0d61745
tozn3877 <torindale05@gmail.com>
2025-10-05 12:32:32 +1000

    add q2 code

--- prac_03/files.py
+++ prac_03/files.py
@@ -13,9 +13,10 @@ out_file.close()
 #2. In the same file, but as if it were a separate program, write code that opens "name.txt" and reads the name (as above) then prints (note the exact output),
 #Hi Bob! (or whatever the name is in the file). Do not simply print the user's input variable!
 #Use open and close for this question.
-
-
-
+in_file = open("name.txt", "r")
+name = in_file.read().strip()
+in_file.close()
+print(f"Hi {name}!")
 
 
 # #3. Create a text file called numbers.txt and save it in your prac directory. Put the following three numbers on separate lines in the file and save it:
