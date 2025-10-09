9fed31e885b8d9f429e8d86f4e62dd7ea0996a10
tozn3877 <torindale05@gmail.com>
2025-10-09 15:02:35 +1000

    add answers to second set of questions

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -9,3 +9,17 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # 7 in numbers - False
 # "3" in numbers - False
 # numbers + [6, 5, 3] - [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+
+#1.
+numbers[0] = "ten"
+
+#2.
+numbers[-1] = 1
+
+#3.
+print(numbers[2:])
+
+#4.
+print(9 in numbers)
+
