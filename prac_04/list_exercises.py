01bbd5632acb533cfca489bbce145e0f6f86f0d5
tozn3877 <torindale05@gmail.com>
2025-10-10 15:02:40 +1000

    add number input function

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -8,6 +8,17 @@ usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'B
 
 
 def main():
+    numbers = get_five_numbers()
+
+
+
+def get_five_numbers():
+    """Prompt for 5 numbers and return them as a list."""
+    numbers = []
+    for _ in range(5):
+        number = float(input("Number: "))
+        numbers.append(number)
+    return numbers
 
 
 
