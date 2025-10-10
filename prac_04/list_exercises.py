8feaf42f92ddee3a2fbe9d3f75ff540e1770e625
tozn3877 <torindale05@gmail.com>
2025-10-10 14:38:29 +1000

    create file, add basic given data

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,16 @@
+"""
+CP1404/CP5632 - Practical
+list exercises: basic list operations and simple username check
+"""
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+
+def main():
+
+
+
+
+
+main()
