2a30af6ce3432533b245da567132423c62f686db
tozn3877 <torindale05@gmail.com>
2025-10-10 14:28:06 +1000

    add function for displaying data and change variables to have more info

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,11 +8,10 @@ FILENAME = "subject_data.txt"
 
 def main():
     """Load records from file and print the nested list."""
-    data = load_data(FILENAME)
-    print(data)
+    file_data = load_file_data(FILENAME)
+    display_subject_details(file_data)
 
-
-def load_data(filename=FILENAME):
+def load_file_data(filename=FILENAME):
     """Return  records as a nested list: [['CP1401', 'Ada Lovelace', 192],['CP1404', 'Alan Turing', 98]]"""
     subjects = []
     with open(filename) as input_file:
@@ -20,5 +19,11 @@ def load_data(filename=FILENAME):
             code, lecturer, students = line.strip().split(',')
             subjects.append([code, lecturer, int(students)])
     return subjects
+
+def display_subject_details(file_data):
+    """Display each subject in the format: CP1401 is taught by Ada Lovelace and has 192 students."""
+    for code, lecturer, students in file_data:
+        print(f"{code} is taught by {lecturer} and has {students} students")
+
 
 main()
