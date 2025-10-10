c01a07dbf58c9be2ed126450f8abb99547368b8d
tozn3877 <torindale05@gmail.com>
2025-10-10 15:53:43 +1000

    add function for picks

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -3,11 +3,21 @@ CP1404/CP5632 - Practical
 quick picks lottery generator (starter)
 """
 
+import random
 
-def main()
+NUMBERS_PER_PICK =6
+SMALLEST_PICK = 1
+LARGEST_PICK = 45
+
+def main():
+    """Prompt for how many quick picks to generate"""
+    number_of_quick_picks = int(input("How many quick picks? "))
+    for i in range(number_of_quick_picks):
+        quick_pick = generate_pick
 
 
 
+def generate_pick():
 
 
 main()
