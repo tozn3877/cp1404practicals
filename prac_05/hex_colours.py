87533353ad39b1f4c6f151dfd65bb77cd18c884c
tozn3877 <torindale05@gmail.com>
2025-10-15 16:38:26 +1000

    Create file, added colour list

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,18 @@
+"""
+CP1404/CP5632 - Practical
+Hex colours lookup
+"""
+
+HEX_COLOURS = {
+    "absolute zero": "#0048ba",
+    "acid green": "#b0bf1a",
+    "aliceblue": "#f0f8ff",
+    "alizarin crimson": "#e32636",
+    "amaranth": "#e52b50",
+    "amber": "#ffbf00",
+    "amethyst": "#9966cc",
+    "antiquewhite": "#faebd7",
+    "antiquewhite1": "#ffefdb",
+    "antiquewhite2": "#eedfcc",
+}
+
