4227ba85f7b8ee396293b021844c7dfebbb18955
tozn3877 <torindale05@gmail.com>
2025-10-15 16:29:35 +1000

    Change to EAFP

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -16,9 +16,9 @@ print(CODE_TO_NAME)
 
 state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
+    try:
         print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+    except KeyError:
         print("Invalid short state")
     state_code = input("Enter short state: ").upper()
 
