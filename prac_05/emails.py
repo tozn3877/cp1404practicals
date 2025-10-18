dbd4b987fb623e1169fd84aabc2e18358d771dec
tozn3877 <torindale05@gmail.com>
2025-10-18 12:43:58 +1000

    Add confirmation message

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -2,7 +2,7 @@
 CP1404/CP5632 - Practical
 Emails
 Estimate: 15 minutes
-Actual:   25 minutes
+Actual:    minutes
 """
 
 
@@ -14,10 +14,12 @@ def get_name_from_email(email):
 
 
 def main():
-    """Prompt for one email and display name"""
+    """Prompt for one email and display name, ask if right"""
     email = input("Email: ")
     name = get_name_from_email(email)
-    print(name)
-
+    confirmation = input(f"Is your name {name}? (Y/n) ").strip().lower()
+    if confirmation not in ("", "y", "yes"):
+        name = input("Name: ").strip()
+    print(f"{name} ({email})")
 
 main()
