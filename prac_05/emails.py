d129815acaea7c09c7b47e67a80354ff28733633
tozn3877 <torindale05@gmail.com>
2025-10-18 12:55:38 +1000

    Add dictionary to store multiple emails and names and printout for it

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -2,24 +2,29 @@
 CP1404/CP5632 - Practical
 Emails
 Estimate: 15 minutes
-Actual:    minutes
+Actual:   47 minutes
 """
 
 
 def get_name_from_email(email):
-    """Return capitalised name guessed from given email address."""
+    """Return capitalised name from given email address."""
     username = email.split("@")[0]
     username = username.replace(".", " ")
     return " ".join(word.capitalize() for word in username.split())
 
 
 def main():
-    """Prompt for one email and display name, ask if right"""
-    email = input("Email: ")
-    name = get_name_from_email(email)
-    confirmation = input(f"Is your name {name}? (Y/n) ").strip().lower()
-    if confirmation not in ("", "y", "yes"):
-        name = input("Name: ").strip()
-    print(f"{name} ({email})")
+    """Prompt for emails until blank; confirm/correct names; then display all."""
+    emails_to_names = {}
+    email = input("Email: ").strip()
+    while email != "":
+        name = get_name_from_email(email)
+        confirmation = input(f"Is your name {name}? (Y/n) ").strip().lower()
+        if confirmation not in ("", "y"):
+            name = input("Name: ").strip()
+        emails_to_names[email] = name
+        email = input("Email: ").strip()
 
+    for email, name in emails_to_names.items():
+           print(f"{name} ({email})")
 main()
